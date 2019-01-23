17f9131c0326438cbe32b320885101043025c49d
Krystian Szczepanowski <krystian.szczepanowski@softwaremind.com>
2019-01-23 15:40:52 +0100

    Komentarze TODO

--- Palindrom/Palindrom/Program.cs
+++ Palindrom/Palindrom/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//TODO Zamien pętle while na np for - w algorytmach o liniowej złożoności raczej sie nie powinno stosować while
+//TODO TE SAME UWAGI JAK DO KODU Z SILNIA
+//TODO Algorytm działa, ale pomyśl czy nie możnaby go przyspieszyć trochę. W tym momencie wykona tyle operacji ile damy mu znaków, czy to aby konieczne? :)
+//TODO Jak commitujesz zmiany w jakims projekcie, to staraj sie nie dołączać projektów BIN/Object
+
 
 namespace Palindrom
 {

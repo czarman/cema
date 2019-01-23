6a8a86b331409b189254e7418fcf72b62ae3f14b
Krystian Szczepanowski <krystian.szczepanowski@softwaremind.com>
2019-01-23 15:04:53 +0100

    Komentarze po Review

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//TODO Zawsze usuwaj nieuzywane usingi - jak masz R# to skrot Ctrl + E + C i wyczysci wszystko
+
 
 namespace Factorial
 {
@@ -11,6 +13,13 @@ namespace Factorial
         static int liczba;
         static long wynik;
 
+        //TODO Trochę tu trzeba posprzątać. To jest prosta aplikacja ale mimo wszystko standardy trzeba zachowywać i tak.
+        //TODO Stworz klasę Silnia ktora będzie miała 2 metody. GetRecursiveValue i GetLoopValue, ktore to będą liczyć tą silnie.
+        //TODO Lub też patrząc bardziej obiektowo. Interfejs IFactorial z metodą CountFactorial i dwie implementacje tego interfejsu LoopFactorial i RecursiveFactorial.
+        //TODO W metodzie main skoro już jesteśmy w konsolowej aplikacji powinno być sterowanie, tworzenie potrzebnych obiektów.
+        //TODO Kod powinien być samodokumentujący się. Popraw nazwy zmiennych i metod - method2/liczba to nie są dobre nazwy. Już lepiej byłoby PoliczSilnięRekurencyjnie i wynikCzesciowy
+        //TODO Formatuj kod - jak wyżej CTRL + E + C sformatuje ci ładnie kod.
+        //TODO Wprowadz te poprawki co pisalem i sprawdze jeszcze raz :)
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj liczbę ");

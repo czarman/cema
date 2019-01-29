93c48592722cbc1e1f71fc1f7324ca88d0e26f57
cema <c.machowski@gmail.com>
2019-01-29 15:32:51 +0100

    rework according to codereview

---
+++ Palindrom/Palindrom/IfPalindrome.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Palindrom
+{
+    internal class IfPalindrome
+    {
+        public static void If_Palindrome(string ToCheck)
+        {
+            var lenght = ToCheck.Length;
+            var fromEnd = lenght - 1;
+            var sentense = ToCheck.ToCharArray();
+
+            var half = lenght/2;
+
+
+            for (var j = 0; j < half; j++)
+            {
+                if (sentense[j] == sentense[fromEnd])
+                {
+                }
+                else
+                {
+                    Console.WriteLine("To nie jest palindrom");
+                    return;
+                }
+                ;
+                fromEnd--;
+            }
+
+            Console.WriteLine("To jest palindrom");
+
+
+            //old version
+            /*
+             *          string Backward = null;
+                        int a = 0;
+                        foreach (var single in Backward)
+                        {
+                            single = sentense[a];
+                        }
+
+                        /* while (i >= 0)
+                        {
+                            Backward = Backward + ToCheck.Substring(i, 1);
+                            i--;
+
+                        }
+
+                        if (ToCheck == Backward)
+                        {
+                            Console.WriteLine("To jest Palindrom");
+                        }
+                        else
+                        {
+                            Console.WriteLine("To nie jest Palindrom");
+                        }
+                        */
+        }
+    }
+}
--- Palindrom/Palindrom/Program.cs
+++ Palindrom/Palindrom/Program.cs
@@ -1,55 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 //TODO Zamien pętle while na np for - w algorytmach o liniowej złożoności raczej sie nie powinno stosować while
 //TODO TE SAME UWAGI JAK DO KODU Z SILNIA
 //TODO Algorytm działa, ale pomyśl czy nie możnaby go przyspieszyć trochę. W tym momencie wykona tyle operacji ile damy mu znaków, czy to aby konieczne? :)
 //TODO Jak commitujesz zmiany w jakims projekcie, to staraj sie nie dołączać projektów BIN/Object
 
-
 namespace Palindrom
 {
-    class Program
+    internal class Program
     {
-
         public static string ToCheck;
 
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
             Console.WriteLine("Wpisz tekst:");
             ToCheck = Console.ReadLine();
-            CzyPalindrom(ToCheck.ToLower());
+            IfPalindrome.If_Palindrome(ToCheck.ToLower());
             Console.WriteLine("Wcisnij klawisz zakończyć");
             Console.ReadKey();
-
-        }
-
-        public static void CzyPalindrom(string ToCheck)
-        {
-            int lenght = ToCheck.Length;
-            string Backward =null;
-            int i = lenght - 1;
-            while(i>=0)
-            {
-                Backward = Backward + ToCheck.Substring(i, 1);
-                i--;
-                
-            }
-            
-            if (ToCheck == Backward)
-            {
-                Console.WriteLine("To jest Palindrom");
-            }
-            else
-            {
-                Console.WriteLine("To nie jest Palindrom");
-            }
-
-            
         }
     }
 }
